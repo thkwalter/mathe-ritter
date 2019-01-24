d6c2cd366d19801b654bd8d9c7c45350085478a5
thkwalter <th.k.walter@googlemail.com>
2019-01-24 22:32:49 +0100

    Die Anzahl der richtigen Ergebnisse wird angezeigt.
    https://github.com/thkwalter/mathe-ritter/projects/1#card-16848947

--- src/app/einmaleins/einmaleins.component.ts
+++ src/app/einmaleins/einmaleins.component.ts
@@ -8,56 +8,77 @@ import { ZufallszahlService} from '../zufallszahl.service';
 })
 export class EinmaleinsComponent implements OnInit
 {
-  ergebnisKorrekt : boolean;
+   ergebnisKorrekt : boolean;
 
-  meldung : string;
+   meldung : string;
 
-  faktor1 : number
-  faktor2 : number;
-  korrektesErgebnis : number;
-  eingegebenesErgebnis : number;
+   faktor1 : number
+   faktor2 : number;
+   korrektesErgebnis : number;
+   eingegebenesErgebnis : number;
 
-  constructor(private zufallszahlService: ZufallszahlService)
-  {
-    this.meldung = "Löse bitte die folgende Aufgabe:"
+   zaehlerKorrekt : number;
+   zaehlerFehlerhaft : number;
 
-    // Dies bewirkt, dass die Meldung in der normalen Textfarbe angezeigt wird.
-    this.ergebnisKorrekt = true;
-  }
+   constructor(private zufallszahlService: ZufallszahlService) {}
 
-  ngOnInit()
-  {
-    this.meldung = "Löse bitte die folgende Aufgabe:"
-    this.neueAufgabeErstellen();
-  }
+   ngOnInit()
+   {
+      // Ein neues Spiel wird begonnen.
+      this.starten();
+   }
 
-  /**
-   * Kontrolliert das Ergebnis, erzeugt eine Meldung und ggf. eine  neue Aufgabe.
-   */
-  kontrollieren()
-    {
-    this.ergebnisKorrekt = (this.eingegebenesErgebnis == this.korrektesErgebnis);
+   /**
+    * Kontrolliert das Ergebnis, erzeugt eine Meldung und ggf. eine  neue Aufgabe.
+    */
+   kontrollieren()
+      {
+      this.ergebnisKorrekt = (this.eingegebenesErgebnis == this.korrektesErgebnis);
 
-    this.eingegebenesErgebnis = null;
+      this.eingegebenesErgebnis = null;
 
-    if (this.ergebnisKorrekt)
-    {
-      this.meldung = "Dein Ergebnis ist richtig! Löse bitte noch folgende Aufgabe:"
+      // Falls das eingegebene Ergebnis korrekt ist, ...
+      if (this.ergebnisKorrekt)
+      {
+         this.meldung = "Dein Ergebnis ist richtig! Löse bitte noch folgende Aufgabe:"
+         this.zaehlerKorrekt++;
+         this.neueAufgabeErstellen();
+      }
+
+      // Falls das eingegebene Ergebnis fehlerhaft ist, ...
+      else
+      {
+         this.meldung = "Dein Ergebnis ist leider falsch! Bitte versuche es noch einmal."
+         this.zaehlerFehlerhaft++;
+      }
+   }
+
+   /**
+    * Startet ein neues Spiel, d.h. die Zähler werden zurückgesetzt.
+    */
+   starten()
+   {
+      // Die Zähler werden initial auf 0 gestellt.
+      this.zaehlerKorrekt = 0;
+      this.zaehlerFehlerhaft = 0;
+
+      // Dies bewirkt, dass die Meldung in der normalen Textfarbe angezeigt wird.
+      this.ergebnisKorrekt = true;
+
+      // Die initiale Melung wird angezeigt.
+      this.meldung = "Löse bitte die folgende Aufgabe:"
+
+      // Die erste Aufgabe wird bestimmt.
       this.neueAufgabeErstellen();
-    }
-    else
-    {
-      this.meldung = "Dein Ergebnis ist leider falsch! Bitte versuche es noch einmal."
-    }
-  }
+   }
 
-  /**
-   * Erstellt eine neue Aufgabe.
-   */
-  neueAufgabeErstellen()
-  {
-    this.faktor1 = this.zufallszahlService.getZufallszahl();
-    this.faktor2 = this.zufallszahlService.getZufallszahl();
-    this.korrektesErgebnis = this.faktor1 * this.faktor2;
-  }
+   /**
+    * Erstellt eine neue Aufgabe.
+    */
+   neueAufgabeErstellen()
+   {
+      this.faktor1 = this.zufallszahlService.getZufallszahl();
+      this.faktor2 = this.zufallszahlService.getZufallszahl();
+      this.korrektesErgebnis = this.faktor1 * this.faktor2;
+   }
 }
