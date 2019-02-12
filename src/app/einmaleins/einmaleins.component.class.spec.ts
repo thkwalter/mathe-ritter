90f2a3d42432a2794385a083d39a487ebc4b028d
thkwalter <th.k.walter@googlemail.com>
2019-02-12 22:23:15 +0100

    1. Test für die kontrollieren()-Methode geschrieben.
    https://github.com/thkwalter/mathe-ritter/projects/2#card-17459679

--- src/app/einmaleins/einmaleins.component.class.spec.ts
+++ src/app/einmaleins/einmaleins.component.class.spec.ts
@@ -3,17 +3,30 @@ import { ZufallszahlService} from '../zufallszahl.service';
 
 describe('EinmaleinsComponent::', () =>
 {
-   it('starten()', () =>
+   let component : EinmaleinsComponent;
+   let service : ZufallszahlService;
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+   beforeEach(() =>
    {
       // Der ZufallszahlService und die EinmaleinsComponent werden erzeugt.
-      const service = new ZufallszahlService();
-      const component = new EinmaleinsComponent(service);
+      service = new ZufallszahlService();
+      component = new EinmaleinsComponent(service);
+   });
 
+// =====================================================================================================================
+// =====================================================================================================================
+
+   it('starten()', () =>
+   {
       // Die von der Methode aufgerufenen Methoden werden durch einen Spy ersetzt.
       spyOn(component, 'fokusAufEingabefeldSetzen');
       spyOn(component, 'neueAufgabeErstellen');
 
-      // Die Attribute der Komponente werden initialisiert.
+      // Die Attribute der Komponente werden mit Werten initialisiert, die nicht den Werten nach Aufruf der Methode
+      // starten() entsprechen.
       component.zaehlerKorrekt = 5;
       component.zaehlerFehlerhaft = 1;
       component.ergebnisKorrekt = false;
@@ -23,15 +36,41 @@ describe('EinmaleinsComponent::', () =>
       // Die zu testende Methode wird ausgeführt.
       component.starten();
 
-      // Die Attribute der Komponente werden geprüft.
+      // Die starten()-Methode sollte mehrere Attribute wieder auf ihre Werte bei Start eines neues Spiels setzen.
       expect(component.zaehlerKorrekt).toBe(0);
       expect(component.zaehlerFehlerhaft).toBe(0);
       expect(component.ergebnisKorrekt).toBeTruthy;
       expect(component.meldung).toMatch("Löse bitte die folgende Aufgabe:");
       expect(component.eingegebenesErgebnis).toBeNull();
 
-      // Es wird geprüft, ob die Spies aufgerufen worden sind.
+      // Die starten()-Methode sollte den Fokus wieder auf das Eingangsfeld setzen und eine neue Aufgabe auswürfeln.
       expect(component.fokusAufEingabefeldSetzen).toHaveBeenCalled();
       expect(component.neueAufgabeErstellen).toHaveBeenCalled();
+   });
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+   it('kontrollieren(), falls eingegebenesErgebnis == null', () =>
+   {
+      // Die von der Methode aufgerufenen Methoden werden durch einen Spy ersetzt.
+      spyOn(component, 'fokusAufEingabefeldSetzen');
+
+      // Die Attribute der Komponente werden initialisiert.
+      component.zaehlerKorrekt = 5;
+      component.zaehlerFehlerhaft = 1;
+
+      // Die vorgegebene Testbedingung (eingegebenesErgebnis == null) wird hergestellt
+      component.eingegebenesErgebnis == null;
+
+      // Die zu testende Methode wird ausgeführt.
+      component.kontrollieren();
+
+      // Unter der vorgegebenen Testbedingung sollte die kontrollieren()-Methode ausschließlich den Fokus wieder auf
+      // das Eingabefeld setzen. Insbesondere sollten sich also die Werte für die korrekten und fehlerhaften Antworten
+      // nicht verändern.
+      expect(component.zaehlerKorrekt).toBe(5);
+      expect(component.zaehlerFehlerhaft).toBe(1);
+      expect(component.fokusAufEingabefeldSetzen).toHaveBeenCalled();
    });
 });
