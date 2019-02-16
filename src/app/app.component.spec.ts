f2defdb4cf262e02fc07a1449092f6dcd7ee7d12
thkwalter <th.k.walter@googlemail.com>
2019-02-16 15:13:00 +0100

    Test für den DOM der AppComponent geschrieben.
    https://github.com/thkwalter/mathe-ritter/projects/2#card-17714403

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,42 +1,52 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Component } from '@angular/core';
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
+describe('AppComponent', () =>
+{
+   let component: AppComponent;
+   let fixture: ComponentFixture<AppComponent>;
 
-      ],
-      declarations: [
-        AppComponent,
-        EinmaleinsComponentMock
-      ],
-      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
-    }).compileComponents();
-  }));
+   beforeEach(() =>
+   {
+      TestBed.configureTestingModule
+      ({
+         declarations:
+            [
+            AppComponent,
+            EinmaleinsComponentMock
+            ],
+         schemas:
+            [
+            CUSTOM_ELEMENTS_SCHEMA
+            ]
+      }).compileComponents();
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.debugElement.componentInstance;
+   });
 
-  it(`should have as title 'mathe-ritter'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Mathe-Ritter');
-  });
+// =====================================================================================================================
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('span').textContent).toContain('Mathe-Ritter');
-  });
+   it('startet', () =>
+   {
+      expect(component).toBeDefined();
+   });
+
+// =====================================================================================================================
+
+   it('zeigt in der Toolbar den Titel Mathe-Ritter an', () =>
+   {
+      fixture.detectChanges();
+      const wurzelElement : HTMLElement = fixture.debugElement.nativeElement;
+      expect(wurzelElement.querySelector('mat-toolbar span#titel').textContent).toBe('Mathe-Ritter');
+   });
 });
+
+// =====================================================================================================================
+// =====================================================================================================================
 
 @Component
 ({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+@Component
+({
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Mathe-Ritter';
+export class AppComponent
+{
+   titel = 'Mathe-Ritter';
 }
